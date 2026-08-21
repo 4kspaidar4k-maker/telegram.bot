de9e1d233a7b7858c81eac34152d9b9e2586b840
4kspaidar4k-maker <4kspaidar4k@gmail.com>
2026-08-21 03:43:08 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require("node-telegram-bot-api");
+const { TelegramBot } = require("node-telegram-bot-api");
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const crypto = require("crypto");
